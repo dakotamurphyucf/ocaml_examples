8aafae627b3e4e229755b8f01754fa26c29b97a4
Dakota Murphy <dakotamurphyucf@gmail.com>
2021-08-26 01:16:37 -0400

    Update __init__.py

--- python_ocaml/__init__.py
+++ python_ocaml/__init__.py
@@ -1,11 +1,14 @@
-from ocaml_module import (
-    hello
-)
 import os
 from ctypes import PyDLL, RTLD_GLOBAL, c_char_p
 
-curdir = dir_path = os.path.dirname(os.path.realpath(__file__))
+cursor = dir_path = os.path.dirname(os.path.realpath(__file__))
 dll = PyDLL(f"{curdir}/pywrap.so", RTLD_GLOBAL)
 argv_t = c_char_p * 2
 argv = argv_t("pywrap.so".encode("utf-8"), None)
 dll.caml_startup(argv)
+
+
+
+from ocaml_module import (
+    hello
+)
